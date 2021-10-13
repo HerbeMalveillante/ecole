fc73d9983a4390aed333e1eb6ca94db8c687fcd0
Herbe Malveillante <herbemalveillante@gmail.com>
2021-10-13 18:12:02 +0200

    cryptage

--- TD6/cryptage.py
+++ TD6/cryptage.py
@@ -13,19 +13,53 @@ def verif(s):
     if len(s)%2 != 0:
         return False
     
-    lastLetter = None
+    existingLetters = []
     for i in stringPair:
-        if i == lastLetter:
-            return False
-        else :
-            lastLetter = i
+        if i in existingLetters:
+            return False 
+        else : 
+            existingLetters.append(i)
     
-    lastLetter = None
+    existingLetters = []
     for i in stringImpair:
-        if i == lastLetter:
-            return False
-        else :
-            lastLetter = i
+        if i in existingLetters:
+            return False 
+        else : 
+            existingLetters.append(i)
     
     return True
 
+def saisieCle():
+    while True:
+        entree = input("entrez une clef :")
+        if verif(entree) :
+            return entree
+
+def eclate(key):
+    stringPair = ""
+    stringImpair = ""
+
+    # création des deux trucs
+    for i in range(len(key)):
+        if i %2 == 0: # pair
+            stringPair += key[i]
+        else : # impair
+            stringImpair += key[i]
+    
+    return (stringPair, stringImpair)
+
+def cryptage(s, cle):
+
+    pair, impair = eclate(cle)
+
+    crypte = ""
+
+    for i in s : 
+        if i in pair :
+            crypte += impair[pair.index(i)]
+        else :
+            crypte += i
+
+    return crypte 
+
+print(cryptage("maman a tort","myturtleiskind"))
