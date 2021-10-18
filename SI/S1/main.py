9e0c627e40a73f8092a7c7ac5d5add71898cd273
Herbe Malveillante <herbemalveillante@gmail.com>
2021-10-18 08:43:55 +0200

    adding BME s2

--- SI/S1/main.py
+++ SI/S1/main.py
@@ -1,17 +1,26 @@
 import pycom
 import time
+from BME280 import *
 
-pycom.heartbeat(False)
 
-delay = 1
+def pyms():
 
-while True:
-    # colors in hexadecimal (0xRRGGBB)
-    pycom.rgbled(0xFF0000)  # Red
-    time.sleep(delay)
-    pycom.rgbled(0x00FF00)  # Green
-    time.sleep(delay)
-    pycom.rgbled(0x0000FF)  # Blue
-    time.sleep(delay)
+    pycom.heartbeat(False)
 
-    delay -= 0.01
+    delay = 1
+
+    while True:
+        # colors in hexadecimal (0xRRGGBB)
+        pycom.rgbled(0xFF0000)  # Red
+        time.sleep(delay)
+        pycom.rgbled(0x00FF00)  # Green
+        time.sleep(delay)
+        pycom.rgbled(0x0000FF)  # Blue
+        time.sleep(delay)
+
+        delay -= 0.01
+
+bus_i2c = I2C(0)
+bus_i2c.init(I2C.MASTER, baudrate = 400000)
+adr = bus_i2c.scan()
+print("Adresse périphérique I2C :", adr)
